ec8bc65616dc162a5b3e8bb56f85594f161fc9cb
agql <agustin.gonzalezquel@gmail.com>
2016-02-23 12:38:45 +0100

    Added files via upload
    Revised version from comments from peer review

--- Plot1.R
+++ Plot1.R
@@ -1,19 +1,41 @@
+## Code by Agustin González-Quel - February 2016
+## 
+## This assignment is solved by 2 functions
+##    readAndCleanData - Accepts 1 local filename in CSV format, read it and convert required 
+##        fields to the suitable types. In this case:
+##           Date                - type Date
+##           Global_active_power - numeric
+##           timestamp           - POSIXct
+##
+##    plot1 - print a histogram and saves the chart to a .PNG file
+##
 
+# plot1 function
 plot1 <- function(filename) {
   
+  # Call to auxiliary fucntion to read and clean data.
   dataToPlot <- readAndCleanData(filename)
 
-  hist(dataToPlot$Global_active_power, main = paste("Global Active Power"), col="blue", xlab="Global Active Power (KW)")
+  hist(dataToPlot$Global_active_power, main = paste("Global Active Power"), 
+       col="red", xlab="Global Active Power (Kilowatts)", 
+       cex.lab = 0.8, cex.main = 0.9
+  )
   dev.copy(png, file="plot1.png", width=480, height=480)
   dev.off()
 }
 
+# readAndCleanData function
 readAndCleanData <- function(filename)
 {
+# reads and convert Date
   powCom = read.csv(filename, header=TRUE, sep =";", quote = "\"", na.strings = "?")
   powCom$Date <- as.Date(powCom$Date, format="%d/%m/%Y")
+
+  # Select the suitable dates so further calculation takes less time.
   powComSel <- powCom[(powCom$Date=="2007-02-01") | (powCom$Date=="2007-02-02"),]
+  
   powComSel$Global_active_power <- as.numeric(as.character(powComSel$Global_active_power))
   powComSel <- transform(powComSel, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+  # Returns data ready to be printed.
   powComSel
 }
--- Plot2.R
+++ Plot2.R
@@ -1,9 +1,23 @@
+## Code by Agustin González-Quel - February 2016
+## 
+## This assignment is solved by 2 functions
+##    readAndCleanData - Accepts 1 local filename in CSV format, read it and convert required 
+##        fields to the suitable types. In this case:
+##           Date                - type Date
+##           Global_active_power - numeric
+##           timestamp           - POSIXct
+##
+##    plot2 - prints a plot and saves the chart to a .PNG file
+##
 
+# plot2 function
 plot2 <- function(filename) {
 
+  # Call to auxiliary fucntion to read and clean data.
   dataToPlot <- readAndCleanData(filename)
   
-  plot(dataToPlot$timestamp,dataToPlot$Global_active_power, type="l", xlab="", ylab="Global Active Power (KW)")
+  plot(dataToPlot$Timestamp, dataToPlot$Global_active_power, type="l", xlab="", 
+       ylab="Global Active Power (Kilowatts)", cex.lab = 0.8)
   dev.copy(png, file="plot2.png", width=480, height=480)
   dev.off()
 }
@@ -14,6 +28,6 @@ readAndCleanData <- function(filename)
   powCom$Date <- as.Date(powCom$Date, format="%d/%m/%Y")
   powComSel <- powCom[(powCom$Date=="2007-02-01") | (powCom$Date=="2007-02-02"),]
   powComSel$Global_active_power <- as.numeric(as.character(powComSel$Global_active_power))
-  powComSel <- transform(powComSel, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+  powComSel <- transform(powComSel, Timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
   powComSel
 }
