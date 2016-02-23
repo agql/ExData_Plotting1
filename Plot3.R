dba44046cff4137472889448621a975de9a83303
agql <agustin.gonzalezquel@gmail.com>
2016-02-23 12:45:16 +0100

    Added files via upload
    Updated files considering comments from peers review

--- Plot3.R
+++ Plot3.R
@@ -1,13 +1,30 @@
+## Code by Agustin González-Quel - February 2016
+## 
+## This assignment is solved by 2 functions
+##    readAndCleanData - Accepts 1 local filename in CSV format, read it and convert required 
+##        fields to the suitable types. In this case:
+##           Date                  - type Date
+##           Sub_metering_1(2 & 3) - numeric
+##           timestamp             - POSIXct
+##
+##    plot3 - prints the three lines in a chart to screen and to a .PNG file
+##
 
+# plot3 function
 plot3 <- function(filename) {
 
+  # Call to auxiliary fucntion to read and clean data.
   dataToPlot <- readAndCleanData(filename)
   
-  plot(dataToPlot$timestamp,dataToPlot$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+  png(filename = "plot3.png", width = 480, height = 480, units = "px", pointsize = 12)
+  par(mar=c(2,4,1,1))
+  plot(dataToPlot$timestamp,dataToPlot$Sub_metering_1, type="l", xlab="", 
+       ylab="Energy sub metering", cex.lab = 0.8, cex.axis = 0.8)
   lines(dataToPlot$timestamp,dataToPlot$Sub_metering_2,col="red")
   lines(dataToPlot$timestamp,dataToPlot$Sub_metering_3,col="blue")
-  legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), lwd=c(1,1))
-  dev.copy(png, file="plot3.png", width=480, height=480)
+  legend("topright", col=c("black","red","blue"), 
+         c("Sub_metering_1","Sub_metering_2", "Sub_metering_3") )
+  
   dev.off()
 }
 
--- Plot4.R
+++ Plot4.R
@@ -1,33 +1,42 @@
+## Code by Agustin González-Quel - February 2016
+## 
+## This assignment is solved by 2 functions
+##    readAndCleanData - Accepts 1 local filename in CSV format, read it and convert required 
+##        fields to the suitable types. In this case:
+##           Date                  - type Date
+##           Global_active_power   - numeric
+##           Global_reactive_power - numeric
+##           Voltage               - numeric
+##           Sub_metering_1(2 & 3) - numeric
+##           timestamp             - POSIXct
+##
+##    plot4 - prints four charts in a matrix
+##
 
+# plot3 function
 plot4 <- function(filename) {
 
   dataToPlot <- readAndCleanData(filename)
-  
+
+  png(filename = "plot4.png", width = 480, height = 480, units = "px", pointsize = 12)
   par(mfrow=c(2,2))
   
-  ## 
+  # 
   plot(dataToPlot$timestamp,dataToPlot$Global_active_power, type="l", xlab="", ylab="Global Active Power")
-  ##
+  #
   plot(dataToPlot$timestamp,dataToPlot$Voltage, type="l", xlab="datetime", ylab="Voltage")
   
-  ##PLOT 3
+  # Plot 3
   plot(dataToPlot$timestamp,dataToPlot$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
   lines(dataToPlot$timestamp,dataToPlot$Sub_metering_2,col="red")
   lines(dataToPlot$timestamp,dataToPlot$Sub_metering_3,col="blue")
-  legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), bty="n", cex=.5) #bty removes the box, cex shrinks the text, spacing added after labels so it renders correctly
-  
-  #PLOT 4
+  legend("topright", col=c("black","red","blue"), 
+         c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),
+         cex=.5, lty=c(1,1), lwd=c(1,1)) 
+
+  #Plot 4
   plot(dataToPlot$timestamp,dataToPlot$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
   
-  #OUTPUT
-  dev.copy(png, file="plot4.png", width=480, height=480)
-  dev.off()
-  
-  
-  
-  
-  
-  dev.copy(png, file="plot4.png", width=480, height=480)
   dev.off()
 }
 
